9feced8ea84f7769e3403b08b8eb413a9a2d6932
Kondash Julia <juliakondash@gmail.com>
2019-05-22 13:23:32 +0300

    post2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190521213758) do
+ActiveRecord::Schema.define(version: 20190522094345) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20190521213758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
